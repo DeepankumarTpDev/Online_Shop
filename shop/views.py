50068a698f12dde32e5d6d98c214b0fda81d57b1
Deepankumar <deepankumar@testpress.in>
2024-09-12 07:42:10 +0000

    Add Cart with Add,Update and Remove functionality along with Detail Page

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-
+from cart.forms import CartAddProductForm
 # Create your views here.
 from .models import Category, Product
 def product_list(request, category_slug=None):
@@ -18,4 +18,5 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product': product,'cart_product_form': cart_product_form})
