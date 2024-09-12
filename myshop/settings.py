adbea2e9ebfbe956e1c4b942cb92965d37905bf7
Deepankumar <deepankumar@testpress.in>
2024-09-12 12:00:10 +0000

    Use Celery to add Async Tasks like sending E-mail

--- myshop/settings.py
+++ myshop/settings.py
@@ -127,3 +127,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'deepankumar6748@gmail.com'
+EMAIL_HOST_PASSWORD = '##########'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
